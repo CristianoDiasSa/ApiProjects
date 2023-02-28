cd1b030fc6fec54a06c7677375fdc1ab7ccb95f0
Cristiano Dias Sa <Cristiano_ie@outlook.com>
2023-02-28 16:33:20 +0000

    update on package.json and urlShortener.html

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const MONGO_URI ='mongodb+srv://Cristiano:123@freecodecamptutorial.41hzsrf.mongo
 
 // init project
 var express = require('express');
-var app = express();
+var shortId = require('shortid');
 var mongoose = require('mongoose');
 var mongo = require('mongodb');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 var port = process.env.PORT || 3000;
-app.set('trust proxy', true);
+var app = express();
 
 //This connection will go when deployed
 //mongoose.connect(process.env.MONGO_URI);
 
 //This connection is for local testing
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true },
+);
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -99,14 +102,36 @@ if(date == "Invalid Date"){
 });
 //---------------------------------------------------------------------------------------------------------------------------------------
 // Url Shortener API
+// Create schema and model to store urls
+let ShortUrl = mongoose.model('ShortUrl' , new mongoose.Schema({
+  'original_url': String,
+  'short_url': String,
+  'id' : String
+}));
 // Mounting body-parser
-var jsonParser = bodyParser.json();
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 // Post request
-app.post("/api/shorturl", jsonParser, (req, res)=>{
-  res.json({
-    'success': 'Url processed succesfully!'
+app.post("/api/shorturl/", (req, res)=>{
+  let original_url = req.body.url;
+  let url_id = shortId.generate();
+  let newShortUrl = url_id;
+  
+  let newUrl = new ShortUrl({
+    "original_url": original_url,
+    "short_url": __dirname + '/api/shorturl/' + url_id,
+    "id": url_id
   });
-
+  newUrl.save(function(error, done){
+    if(error) return console.log (error);
+    console.log('insertion successfull');
+    res.json({
+      "original_url": newUrl.original_url,
+      "short_url": newUrl.short_url,
+      "id": newUrl.id
+    });
+  });
 });
 
 // End of url shortener API
