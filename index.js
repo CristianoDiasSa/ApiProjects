638ce8ecda0f874aa7bbc44bd85a47076ccc3d38
Cristiano Dias Sa <Cristiano_ie@outlook.com>
2023-03-07 03:13:41 +0000

    Exercise tracker API almost finished

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ var mongoose = require("mongoose");
 var mongo = require("mongodb");
 var port = process.env.PORT || 3000;
 var app = express();
+require("dotenv").config();
 
-//This connection will go when deployed
-//mongoose.connect(process.env.MONGO_URI);
+//This is the database connection for APIs that requires a database, like exercise Tracking API
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("Connected to database");
+  }
+);
 
-//This connection is for local testing
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
+
+// Everytime we work with the body of a Json object we have to app.use this!!!
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
@@ -23,27 +33,142 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Timestamp api html endpoint
-app.get("/timestamp", function (req, res) {
-  res.sendFile(__dirname + "/views/timestamp.html");
+// Exercise Tracker api html endpoint
+app.get("/exerciseTracker", function (req, res) {
+  res.sendFile(__dirname + "/views/exerciseTracker.html");
+});
+
+// UrlShortener api html endpoint
+app.get("/urlShortener", function (req, res) {
+  res.sendFile(__dirname + "/views/urlShortener.html");
 });
 // HeaderParser api html endpoint
 app.get("/headerParser", function (req, res) {
   res.sendFile(__dirname + "/views/headerParser.html");
 });
-// UrlShortener api html endpoint
-app.get("/urlShortener", function (req, res) {
-  res.sendFile(__dirname + "/views/urlShortener.html");
+// Timestamp api html endpoint
+app.get("/timestamp", function (req, res) {
+  res.sendFile(__dirname + "/views/timestamp.html");
 });
-// Exercise Tracker api html endpoint
-app.get("/exerciseTracker", function (req, res) {
-  res.sendFile(__dirname + "/views/exerciseTracker.html");
-});
+
 //---------------------------------------------------------------------------------------------------------------------------------------
+// Exercise Tracker API
+// Firstly we create a mongoose schema to hold the structure that we need, in this case only username will be required
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+  },
+  { versionKey: false }
+);
+
+// After the schema is created, we create a mongoose model that will work with that schema
+// two arguments are required here, the name of the model and the schema it uses
+const User = mongoose.model("User", userSchema);
+
+// Schema for exercises
+const exerciseSchema = mongoose.Schema(
+  {
+    username: String,
+    description: String,
+    duration: Number,
+    date: String,
+    userId: String,
+  },
+  { versionKey: false }
+);
+// Model for exercises
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+// GET request to api/user
+// Display all users
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+// POST request to api/users
+app.post("/api/users", async (req, res) => {
+  const username = req.body.username;
+  // In order to only create a user if that one does not yet exist, we have to check if the value is already there
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    return res.json({
+      error: "That username " + username + " is already taken",
+    });
+  }
+  const user = await User.create({
+    username,
+  });
+  res.json(user);
+});
+// POST to /api/users/:_id/exercises
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const userId = req.body[":_id"];
+  let { description, duration, date } = req.body;
+
+  const foundUser = await User.findById(userId);
+  if (!foundUser) {
+    return res.json({ Message: "No username found!" });
+  }
+  if (!date) {
+    date = new Date();
+  } else {
+    date = new Date(date);
+  }
+  // Now that we already dealt with every parameter, we can create and save a new exercise document
+  // using the exercise schema we just defined
+  Exercise.create({
+    username: foundUser.username,
+    description,
+    duration,
+    date,
+    userId: userId,
+  });
+
+  // This is the response
+  res.json({
+    username: foundUser.username,
+    description,
+    duration,
+    date: date.toDateString(),
+    _id: userId,
+  });
+});
+// GET request to /api/users/:_id/logs
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const userId = req.params._id;
+  let foundUser = await User.findById(userId);
+  if (!foundUser) {
+    res.json({ Message: "No username found!" });
+  }
+  // Here we look for all documents linked to that userId
+  let exercises = await Exercise.find({ userId });
+  // Here we are just excluding the fields we dont want to send back to user
+  exercises = exercises.map((exercise) => {
+    return {
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date,
+    };
+  });
+  // Here we set our response object
+  res.json({
+    username: foundUser.username,
+    count: exercises.length,
+    _id: userId,
+    logs: exercises,
+  });
+});
+// End of exercise tracker API
+//---------------------------------------------------------------------------------------------------------------------------------------
+
 // Url Shortener API
 
 originalUrls = [];
